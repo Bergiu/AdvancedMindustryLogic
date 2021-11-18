afe36541b551cc11acc292d5994789a815b8c4ae
Marco Schlicht <git@privacymail.dev>
2021-11-18 18:43:42 +0100

    preprocessor now outputs a list with a mapping from the processed line number to the real line number with filename

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,38 +1,58 @@
 import re
 import pathlib
-from typing import List, Union
+from typing import List, Union, Dict, Tuple
 
 from src.utils import load_code
 
 
-def preprocess(text: str, filename: str) -> str:
-    text = "\n".join(_preprocess_intern(text, filename))
+LinesOfCode = List[str]
+LineNumber = int
+Filename = str
+CodeLine = Tuple[LineNumber, Filename]
+CodePos = List[CodeLine]
+
+
+
+class File:
+    def __init__(self, filename, lines_of_code):
+        self.filename = filename
+        self.lines_of_code = lines_of_code
+
+
+def preprocess(text: str, filename: str) -> Tuple[str, CodePos]:
+    lines, code_pos = _preprocess_intern(text, filename)
+    text = "\n".join(lines)
     text = repair_eof(text)
-    return text
+    return text, code_pos
 
 
-def _preprocess_intern(text: str, filename: Union[str, pathlib.Path]) -> List[str]:
+def _preprocess_intern(text: str, filename: Union[str, pathlib.Path]) -> Tuple[LinesOfCode, CodePos]:
     return replace_includes(text, filename)
 
 
 def repair_eof(text) -> str:
-    if text[len(text) - 1] is not "\n":
+    if text[len(text) - 1] != "\n":
         text += "\n"
     return text
 
 
-def replace_includes(text: str, filename: Union[str, pathlib.Path]) -> List[str]:
+def replace_includes(text: str, filename: Union[str, pathlib.Path]) -> Tuple[LinesOfCode, CodePos]:
     lines = text.split("\n")
     new_lines = []
+    code_pos: CodePos = []
     for index, line in enumerate(lines):
         line = line.strip()
         res = re.match("^import ([a-zA-Z/_.]+)[ ]?", line)
         if res is not None:
+            new_lines.append("# " + line)
+            code_pos.append((index, filename))
             new_filename = res.group(1)
             relative_file = pathlib.Path(filename).parent.joinpath(new_filename)
             imported_code = load_code(relative_file)
-            processed_code = _preprocess_intern(imported_code, relative_file)
+            processed_code, new_code_pos = _preprocess_intern(imported_code, relative_file)
             new_lines.extend(processed_code)
+            code_pos.extend(new_code_pos)
         else:
             new_lines.append(line)
-    return new_lines
+            code_pos.append((index, filename))
+    return (new_lines, code_pos)
