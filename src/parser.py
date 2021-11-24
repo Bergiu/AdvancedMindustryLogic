6874141fb8d9e0fa71b586462f7097093a2dd383
Marco Schlicht <git@privacymail.dev>
2021-11-24 02:45:04 +0100

    added typehints and wrapping textokens into Token objects

--- src/parser.py
+++ src/parser.py
@@ -44,28 +44,35 @@ codeblock_t = CodeBlockNode
 
 def p_program1(p):
     '''codeblock : line'''
-    p[0] = CodeBlockNode(p, p[1])
+    line: line_t = p[1]
+    p[0] = CodeBlockNode(p, line)
 
 
 def p_program2(p):
     '''codeblock : codeblock line'''
-    p[0] = CodeBlockNode(p, p[2], p[1])
+    codeblock: codeblock_t = p[1]
+    line: line_t = p[2]
+    p[0] = CodeBlockNode(p, line, codeblock)
 
 
 def p_func_param(p):
     '''func_param : LPAREN ids RPAREN'''
-    p[0] = p[2]
+    ids: ids_t = p[2]
+    p[0] = ids
 
 
 def p_ids1(p):
     '''ids : value'''
-    p[0] = [p[1]]
+    value: value_t = p[1]
+    p[0] = [value]
 
 
 def p_ids2(p):
     '''ids : ids COMMA value'''
-    p[1].append(p[3])
-    p[0] = p[1]
+    ids: ids_t = p[1]
+    value: value_t = p[3]
+    ids.append(value)
+    p[0] = ids
 
 
 def p_ids3(p):
@@ -75,22 +82,38 @@ def p_ids3(p):
 
 def p_function(p):
     '''cmd_function : FUNCTION fakeid func_param LCURLY lineend codeblock RCURLY'''
-    p[0] = FunctionNode(p, p[2], p[3], p[6])
+    fakeid: fakeid_t = p[2]
+    func_param: func_param_t = p[3]
+    lcurly: Token = Token(p.slice[4])
+    # TODO: add lineend
+    codeblock: codeblock_t = p[6]
+    rcurly: Token = Token(p.slice[7])
+    p[0] = FunctionNode(p, fakeid, func_param, codeblock, lcurly, rcurly)
 
 
 def p_write(p):
     '''cmd_write : WRITE value fakeid var_int'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    write: Token = Token(p.slice[1])
+    value: value_t = p[2]
+    fakeid: fakeid_t = p[3]
+    var_int: var_int_t = p[4]
+    p[0] = OperationNode(p, write, [value, fakeid, var_int])
 
 
 def p_read(p):
     '''cmd_read : READ fakeid fakeid var_int'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    read: Token = Token(p.slice[1])
+    fakeid1: fakeid_t = p[2]
+    fakeid2: fakeid_t = p[3]
+    var_int: var_int_t = p[4]
+    p[0] = OperationNode(p, read, [fakeid1, fakeid2, var_int])
 
 
 def p_draw(p):
     '''cmd_draw : DRAW draw_instruction'''
-    p[0] = OperationNode(p, p[1], p[2])
+    draw: Token = Token(p.slice[1])
+    draw_instruction: draw_instruction_t = p[2]
+    p[0] = OperationNode(p, draw, draw_instruction)
 
 
 def p_draw_instruction(p):
@@ -105,32 +128,46 @@ def p_draw_instruction(p):
                         | TRIANGLE var_int var_int var_int var_int var_int var_int
                         | IMAGE var_int var_int fakeid var_number var_number null
     '''
-    p[0] = p[1:]
+    subcommand_s: Token = Token(p.slice[1])
+    subcommand: Token = Token(subcommand_s)
+    parameters: List[Union[var_int_t, fakeid_t, var_number_t, null_t]] = p[2:]
+    p[0] = [subcommand] + parameters
 
 
 def p_drawflush(p):
     '''cmd_drawflush : DRAWFLUSH fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    drawflush: Token = Token(p.slice[1])
+    fakeid: fakeid_t = p[2]
+    p[0] = OperationNode(p, drawflush, [fakeid])
 
 
 def p_print(p):
     '''cmd_print : PRINT value'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    print: Token = Token(p.slice[1])
+    value: value_t = p[2]
+    p[0] = OperationNode(p, print, [value])
 
 
 def p_printflush(p):
     '''cmd_printflush : PRINTFLUSH fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    printflush: Token = Token(p.slice[1])
+    fakeid: fakeid_t = p[2]
+    p[0] = OperationNode(p, printflush, [fakeid])
 
 
 def p_getlink(p):
     '''cmd_getlink : GETLINK fakeid var_int'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    getlink: Token = Token(p.slice[1])
+    fakeid: fakeid_t = p[2]
+    var_int: var_int_t = p[3]
+    p[0] = OperationNode(p, getlink, [fakeid, var_int])
 
 
 def p_control(p):
     '''cmd_control : CONTROL control_instruction'''
-    p[0] = OperationNode(p, p[1], p[2])
+    control: Token = Token(p.slice[1])
+    control_instruction: control_instruction_t = p[2]
+    p[0] = OperationNode(p, control, control_instruction)
 
 
 def p_control_instruction(p):
@@ -140,12 +177,17 @@ def p_control_instruction(p):
                            | CONFIGURE fakeid fakeid null null null
                            | COLOR var_number var_number var_number null null
     '''
-    p[0] = p[1:]
+    subcommand_s: Token = Token(p.slice[1])
+    subcommand: Token = Token(subcommand_s)
+    parameters: List[Union[fakeid_t, var_bool_t, null_t, var_number_t]] = p[2:]
+    p[0] = [subcommand] + parameters
 
 
 def p_radar(p):
     '''cmd_radar : RADAR radar_target radar_target radar_target radar_sort fakeid var_int fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    radar: Token = Token(p.slice[1])
+    parameters: List[Union[radar_target_t, radar_sort_t, fakeid_t, var_int_t]] = p[2:]
+    p[0] = OperationNode(p, radar, parameters)
 
 
 def p_radar_target(p):
@@ -158,7 +200,8 @@ def p_radar_target(p):
                     | BOSS
                     | GROUND
     '''
-    p[0] = p[1]
+    subcommand_s: Token = Token(p.slice[1])
+    p[0] = Token(subcommand_s)
 
 
 def p_radar_sort(p):
@@ -168,57 +211,91 @@ def p_radar_sort(p):
                   | ARMOR
                   | MAXHEALTH
     '''
-    p[0] = p[1]
+    subcommand_s: Token = Token(p.slice[1])
+    p[0] = Token(subcommand_s)
 
 
 def p_sensor(p):
     '''cmd_sensor : SENSOR fakeid fakeid fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    sensor: Token = Token(p.slice[1])
+    parameters: List[fakeid_t] = p[2:]
+    p[0] = OperationNode(p, sensor, parameters)
 
 
 def p_set(p):
     '''cmd_set : SET fakeid value'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    set: Token = Token(p.slice[1])
+    fakeid: fakeid_t = p[2]
+    value: value_t = p[3]
+    p[0] = OperationNode(p, set, [fakeid, value])
 
 
 def p_op(p):
     '''cmd_op : OP op_instruction fakeid value value'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    op: Token = Token(p.slice[1])
+    op_instruction: op_instruction_t = p[2]
+    fakeid: fakeid_t = p[3]
+    value1: value_t = p[4]
+    value2: value_t = p[5]
+    p[0] = OperationNode(p, op, [op_instruction, fakeid, value1, value2])
 
 
 def p_op_add(p):
     '''cmd_op_add : fakeid OP_ASSIGN value OP_ADD value'''
-    params = "add {var} {lv} {rv}".format(var=p[1], lv=p[3], rv=p[5])
-    p[0] = OperationNode(p, "op", params.split(" "))
+    op: str = "op"
+    fakeid: fakeid_t = p[1]
+    value1: value_t = p[3]
+    value2: value_t = p[5]
+    params = "add {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
+    p[0] = OperationNode(p, op, params.split(" "))
 
 
 def p_op_sub(p):
     '''cmd_op_sub : fakeid OP_ASSIGN value OP_SUB value'''
-    params = "sub {var} {lv} {rv}".format(var=p[1], lv=p[3], rv=p[5])
-    p[0] = OperationNode(p, "op", params.split(" "))
+    op: str = "op"
+    fakeid: fakeid_t = p[1]
+    value1: value_t = p[3]
+    value2: value_t = p[5]
+    params = "sub {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
+    p[0] = OperationNode(p, op, params.split(" "))
 
 
 def p_op_mul(p):
     '''cmd_op_mul : fakeid OP_ASSIGN value OP_MUL value'''
-    params = "mul {var} {lv} {rv}".format(var=p[1], lv=p[3], rv=p[5])
-    p[0] = OperationNode(p, "op", params.split(" "))
+    op: str = "op"
+    fakeid: fakeid_t = p[1]
+    value1: value_t = p[3]
+    value2: value_t = p[5]
+    params = "mul {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
+    p[0] = OperationNode(p, op, params.split(" "))
 
 
 def p_op_div(p):
     '''cmd_op_div : fakeid OP_ASSIGN value OP_DIV value'''
-    params = "div {var} {lv} {rv}".format(var=p[1], lv=p[3], rv=p[5])
-    p[0] = OperationNode(p, "op", params.split(" "))
+    op: str = "op"
+    fakeid: fakeid_t = p[1]
+    value1: value_t = p[3]
+    value2: value_t = p[5]
+    params = "div {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
+    p[0] = OperationNode(p, op, params.split(" "))
 
 
 def p_op_eq(p):
     '''cmd_op_eq : fakeid OP_ASSIGN value OP_EQ value'''
-    params = "equal {var} {lv} {rv}".format(var=p[1], lv=p[3], rv=p[5])
-    p[0] = OperationNode(p, "op", params.split(" "))
+    op: str = "op"
+    fakeid: fakeid_t = p[1]
+    value1: value_t = p[3]
+    value2: value_t = p[5]
+    params = "equal {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
+    p[0] = OperationNode(p, op, params.split(" "))
 
 
 def p_op_set(p):
     '''cmd_op_set : fakeid OP_ASSIGN value'''
-    p[0] = OperationNode(p, "set", [p[1], p[3]])
+    op: str = "set"
+    fakeid: fakeid_t = p[1]
+    value: value_t = p[3]
+    p[0] = OperationNode(p, op, [fakeid, value])
 
 
 def p_op_instruction(p):
@@ -259,17 +336,24 @@ def p_op_instruction(p):
                       | SQRT
                       | RAND
     '''
-    p[0] = p[1]
+    subcommand_s: Token = Token(p.slice[1])
+    p[0] = Token(subcommand_s)
 
 
 def p_end(p):
     '''cmd_end : END'''
-    p[0] = OperationNode(p, p[1])
+    op: Token = Token(p.slice[1])
+    p[0] = OperationNode(p, op)
 
 
 def p_jump(p):
-    '''cmd_jump : JUMP INT jump_comparison value value'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    '''cmd_jump : JUMP int jump_comparison value value'''
+    op: Token = Token(p.slice[1])
+    int_o: int_t = p[2]
+    jump_comparison: jump_comparison_t = p[3]
+    value1: value_t = p[4]
+    value2: value_t = p[5]
+    p[0] = OperationNode(p, op, [int_o, jump_comparison, value1, value2])
 
 
 def p_jump_comparison(p):
@@ -282,17 +366,22 @@ def p_jump_comparison(p):
                        | STRICTEQUAL
                        | ALWAYS
     '''
-    p[0] = p[1]
+    subcommand: Token = Token(p.slice[1])
+    p[0] = Token(subcommand)
 
 
 def p_ubind(p):
     '''cmd_ubind : UBIND fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    ubind: Token = Token(p.slice[1])
+    fakeid: fakeid_t = p[2]
+    p[0] = OperationNode(p, ubind, [fakeid])
 
 
 def p_ucontrol(p):
     '''cmd_ucontrol : UCONTROL ucontrol_instruction'''
-    p[0] = OperationNode(p, p[1], p[2])
+    ucontrol: Token = Token(p.slice[1])
+    ucontrol_instruction: ucontrol_instruction_t = p[2]
+    p[0] = OperationNode(p, ucontrol, ucontrol_instruction)
 
 
 def p_ucontrol_instruction(p):
@@ -314,17 +403,37 @@ def p_ucontrol_instruction(p):
                             | GETBLOCK var_number var_number fakeid fakeid null
                             | WITHIN var_number var_number var_number var_bool null
     '''
-    p[0] = p[1:]
+    subcommand_s: Token = Token(p.slice[1])
+    subcommand: Token = Token(subcommand_s)
+    parameters: List[Union[fakeid_t, var_bool_t, null_t, var_int_t, var_number_t]] = p[2:]
+    p[0] = [subcommand] + parameters
 
 
 def p_uradar(p):
     '''cmd_uradar : URADAR radar_target radar_target radar_target radar_sort null var_bool fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    uradar: Token = Token(p.slice[1])
+    radar_target1: radar_target_t = p[2]
+    radar_target2: radar_target_t = p[3]
+    radar_target3: radar_target_t = p[4]
+    radar_sort: radar_sort_t = p[5]
+    null: null_t = p[6]
+    var_bool: var_bool_t = p[7]
+    fakeid: fakeid_t = p[8]
+    p[0] = OperationNode(p, uradar, [radar_target1, radar_target2, radar_target3, radar_sort, null, var_bool, fakeid])
 
 
 def p_ulocate(p):
     '''cmd_ulocate : ULOCATE ulocate_find ulocate_group var_bool fakeid var_number var_number var_bool fakeid'''
-    p[0] = OperationNode(p, p[1], p[2:])
+    ulocate: Token = Token(p.slice[1])
+    ulocate_find: ulocate_find_t = p[2]
+    ulocate_group: ulocate_group_t = p[3]
+    var_bool1: var_bool_t = p[4]
+    fakeid: fakeid_t = p[5]
+    var_number1: var_number_t = p[6]
+    var_number2: var_number_t = p[7]
+    var_bool2: var_bool_t = p[8]
+    fakeid: fakeid_t = p[9]
+    p[0] = OperationNode(p, ulocate, [ulocate_find, ulocate_group, var_bool1, fakeid, var_number1, var_number2, var_bool2])
 
 
 def p_ulocate_find(p):
@@ -333,7 +442,7 @@ def p_ulocate_find(p):
                     | SPAWN
                     | DAMAGED
     '''
-    p[0] = p[1]
+    p[0] = Token(p.slice[1])
 
 
 def p_ulocate_group(p):
@@ -348,37 +457,45 @@ def p_ulocate_group(p):
                      | RESUPPLY
                      | REACTOR
     '''
-    p[0] = p[1]
+    p[0] = Token(p.slice[1])
 
 
 def p_noop(p):
     '''cmd_noop : NOOP'''
-    p[0] = OperationNode(p, p[1])
+    noop: Token = Token(p.slice[1])
+    p[0] = OperationNode(p, noop)
 
 
 def p_exec(p):
     '''cmd_exec : EXEC fakeid func_param'''
-    p[0] = ExecNode(p, p[2], p[3])
+    fakeid: fakeid_t = p[2]
+    func_param: func_param_t = p[3]
+    p[0] = ExecNode(p, fakeid, func_param)
 
 
 def p_lineend1(p):
     '''lineend : NEWLINE'''
-    p[0] = CommentNode(p, "")
+    newline: Token = Token(p.slice[1])
+    p[0] = NewLineNode(p, newline)
 
 
 def p_lineend2(p):
     '''lineend : COMMENT NEWLINE'''
-    p[0] = CommentNode(p, p[1])
+    comment: Token = Token(p.slice[1])
+    p[0] = CommentNode(p, comment)
 
 
 def p_line(p):
     '''line : operation lineend'''
-    p[0] = OneLineNode(p, p[1], p[2])
+    operation: operation_t = p[1]
+    lineend: lineend_t = p[2]
+    p[0] = OneLineNode(p, operation, lineend)
 
 
 def p_line1(p):
     '''line : lineend'''
-    p[0] = p[1]
+    lineend: lineend_t = p[1]
+    p[0] = lineend
 
 
 def p_line_error(p):
@@ -416,58 +533,92 @@ def p_operation(p):
                  | cmd_op_eq
                  | cmd_op_set
     '''
-    p[0] = p[1]
+    operation: operation_t = p[1]
+    p[0] = operation
 
 
 def p_var_int(p):
     '''var_int : fakeid
-               | INT
+               | int
     '''
-    p[0] = p[1]
+    out: var_int_t = p[1]
+    p[0] = out
 
 
 def p_var_number(p):
     '''var_number : fakeid
                   | number
     '''
-    p[0] = p[1]
+    out: var_number_t = p[1]
+    p[0] = out
 
 
 def p_var_bool(p):
     '''var_bool : value'''
-    p[0] = p[1]
+    # TODO: müsste hier nicht auch fakeid sein???
+    out: var_bool_t = p[1]
+    p[0] = out
 
 
 def p_null(p):
     '''null : value'''
-    p[0] = p[1]
+    out: null_t = p[1]
+    p[0] = out
 
 
 def p_value(p):
     '''value : fakeid
-             | STRING
+             | string
              | number
-             | BOOL
+             | bool
     '''
-    p[0] = p[1]
+    out: value_t = p[1]
+    p[0] = out
 
 
 def p_number(p):
-    '''number : INT
-              | DOUBLE
+    '''number : int
+              | double
     '''
-    p[0] = p[1]
+    out: number_t = p[1]
+    p[0] = out
 
 
 def p_fakeid(p):
-    '''fakeid : ID
+    '''fakeid : id
               | keyword
     '''
-    p[0] = p[1]
+    out: fakeid_t = p[1]
+    p[0] = out
+
+
+def p_id(p):
+    '''id : ID'''
+    p[0] = Token(p.slice[1])
+
+
+def p_int(p):
+    '''int : INT'''
+    p[0] = Token(p.slice[1])
+
+
+def p_double(p):
+    '''double : DOUBLE'''
+    p[0] = Token(p.slice[1])
+
+
+def p_bool(p):
+    '''bool : BOOL'''
+    p[0] = Token(p.slice[1])
+
+
+def p_string(p):
+    '''string : STRING'''
+    p[0] = Token(p.slice[1])
 
 
 def p_keyword(p):
-    p[0] = p[1]
+    p[0] = Token(p.slice[1])
 
 
 p_keyword.__doc__ = "keyword : " + "\n      | ".join([res for res in reserved.values()])
