6eb97e143b8a21edf2ccf1a25a119ade12c6e6f3
Marco Schlicht <git@privacymail.dev>
2021-11-26 16:01:13 +0100

    using statements for operators

--- src/parser/grammar/operators.py
+++ src/parser/grammar/operators.py
@@ -63,16 +63,6 @@ from src.parser.types import value_t, fakeid_t, statement_t
 from src.nodes import OperationNode, OperationStatementNode
 
 
-def p_op_add(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_ADD value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "add {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
 def p_op_add_reduced(p):
     '''cmd_op_short : fakeid OP_ADD OP_ASSIGN value'''
     op: str = "op"
@@ -88,16 +78,6 @@ def p_op_add_reduced_much(p):
     fakeid: fakeid_t = p[1]
     rv = "1"
     params = "add {var} {lv} {rv}".format(var=fakeid, lv=fakeid, rv=rv)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_sub(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_SUB value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "sub {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
     p[0] = OperationNode(p, op, params.split(" "))
 
 
@@ -119,16 +99,6 @@ def p_op_sub_reduced_much(p):
     p[0] = OperationNode(p, op, params.split(" "))
 
 
-def p_op_mul(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_MUL value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "mul {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
 def p_op_mul_reduced(p):
     '''cmd_op_short : fakeid OP_MUL OP_ASSIGN value'''
     op: str = "op"
@@ -138,183 +108,12 @@ def p_op_mul_reduced(p):
     p[0] = OperationNode(p, op, params.split(" "))
 
 
-def p_op_div(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_DIV value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "div {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
 def p_op_div_reduced(p):
     '''cmd_op_short : fakeid OP_DIV OP_ASSIGN value'''
     op: str = "op"
     fakeid: fakeid_t = p[1]
     value1: value_t = p[4]
     params = "div {var} {lv} {rv}".format(var=fakeid, lv=fakeid, rv=value1)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-
-def p_op_lt(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_LT value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "lessThan {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_lte(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_LTE value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "lessThanEq {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_gt(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_GT value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "greaterThan {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_gte(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_GTE value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "greaterThanEq {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_eq(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_EQ value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "equal {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_not_equal(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_NOTEQ value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "notEqual {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_strict_equal(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_STRICTEQ value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "strictEqual {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_idiv(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_IDIV value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "idiv {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_mod(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_MOD value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "mod {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_pow(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_POW value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "pow {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_shl(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_SHL value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "shl {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_shr(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OP_SHR value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "shr {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_and(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value AND value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "and {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_or(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value OR value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "or {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_xor(p):
-    '''cmd_op_short : fakeid OP_ASSIGN value XOR value'''
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value1: value_t = p[3]
-    value2: value_t = p[5]
-    params = "xor {var} {lv} {rv}".format(var=fakeid, lv=value1, rv=value2)
-    p[0] = OperationNode(p, op, params.split(" "))
-
-
-def p_op_inverse(p):
-    '''cmd_op_short : fakeid OP_ASSIGN OP_INV value'''
-    # TODO: use flip operator
-    op: str = "op"
-    fakeid: fakeid_t = p[1]
-    value: value_t = p[4]
-    params = "xor {var} true value".format(var=fakeid, value=value)
     p[0] = OperationNode(p, op, params.split(" "))
 
 
--- src/parser/grammar/statements.py
+++ src/parser/grammar/statements.py
@@ -56,160 +56,295 @@ def p_statement(p):
 
 
 def p_statement_add(p):
-    '''statement : LPAREN statement OP_ADD statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_ADD statement RPAREN
+                 | statement OP_ADD statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "add"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_sub(p):
-    '''statement : LPAREN statement OP_SUB statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_SUB statement RPAREN
+                 | statement OP_SUB statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "sub"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_div(p):
-    '''statement : LPAREN statement OP_DIV statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_DIV statement RPAREN
+                 | statement OP_DIV statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "div"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_mul(p):
-    '''statement : LPAREN statement OP_MUL statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_MUL statement RPAREN
+                 | statement OP_MUL statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "mul"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_lt(p):
-    '''statement : LPAREN statement OP_LT statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_LT statement RPAREN
+                 | statement OP_LT statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "lessThan"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_lte(p):
-    '''statement : LPAREN statement OP_LTE statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_LTE statement RPAREN
+                 | statement OP_LTE statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "lessThanEq"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_gt(p):
-    '''statement : LPAREN statement OP_GT statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_GT statement RPAREN
+                 | statement OP_GT statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "greaterThan"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_gte(p):
-    '''statement : LPAREN statement OP_GTE statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_GTE statement RPAREN
+                 | statement OP_GTE statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "greaterThanEq"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_eq(p):
-    '''statement : LPAREN statement OP_EQ statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_EQ statement RPAREN
+                 | statement OP_EQ statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "equal"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_not_equal(p):
-    '''statement : LPAREN statement OP_NOTEQ statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_NOTEQ statement RPAREN
+                 | statement OP_NOTEQ statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "notEqual"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_strict_equal(p):
-    '''statement : LPAREN statement OP_STRICTEQ statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_STRICTEQ statement RPAREN
+                 | statement OP_STRICTEQ statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "strictEqual"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_idiv(p):
-    '''statement : LPAREN statement OP_IDIV statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_IDIV statement RPAREN
+                 | statement OP_IDIV statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "idiv"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_mod(p):
-    '''statement : LPAREN statement OP_MOD statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_MOD statement RPAREN
+                 | statement OP_MOD statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "mod"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_pow(p):
-    '''statement : LPAREN statement OP_POW statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_POW statement RPAREN
+                 | statement OP_POW statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "pow"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_shl(p):
-    '''statement : LPAREN statement OP_SHL statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_SHL statement RPAREN
+                 | statement OP_SHL statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "shl"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_shr(p):
-    '''statement : LPAREN statement OP_SHR statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OP_SHR statement RPAREN
+                 | statement OP_SHR statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "shr"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_and(p):
-    '''statement : LPAREN statement AND statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement AND statement RPAREN
+                 | statement AND statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "and"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_or(p):
-    '''statement : LPAREN statement OR statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement OR statement RPAREN
+                 | statement OR statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "or"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_xor(p):
-    '''statement : LPAREN statement XOR statement RPAREN'''
-    value1: statement_t = p[2]
-    value2: statement_t = p[4]
+    '''statement : LPAREN statement XOR statement RPAREN
+                 | statement XOR statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[2]
+        value2: statement_t = p[4]
+    else:
+        value1: statement_t = p[1]
+        value2: statement_t = p[3]
     operation: str = "xor"
     p[0] = StatementNode(p, operation, value1, value2)
 
 
 def p_statement_inverse(p):
-    '''statement : LPAREN OP_INV statement RPAREN'''
-    value1: statement_t = p[3]
+    '''statement : LPAREN OP_INV statement RPAREN
+                 | OP_INV statement
+    '''
+    if len(p) > 4:
+        value1: statement_t = p[3]
+    else:
+        value1: statement_t = p[2]
     value2: str = "true"
     operation: str = "xor"
     p[0] = StatementNode(p, operation, value1, value2)
+
+
+precedence = (
+    ('left', 'OP_IDIV', 'OP_POW'),  # Change order
+    #('left', 'LOR'), # logical or
+    #('left', 'LAND'), # logical and
+    ('left', 'OR'),   # bitwise or
+    ('left', 'XOR'),  # bitwise xor
+    ('left', 'AND'),  # bitwise and
+    ('left', 'OP_EQ', 'OP_NOTEQ', 'OP_STRICTEQ'),
+    ('left', 'OP_LT', 'OP_LTE', 'OP_GT', 'OP_GTE'),
+    ('left', 'OP_SHL', 'OP_SHR'),
+    ('left', 'OP_ADD', 'OP_SUB'),
+    ('left', 'OP_MUL', 'OP_DIV', 'OP_MOD'),
+    ('left', 'OP_INV'),  # TODO bitwise not
+)
